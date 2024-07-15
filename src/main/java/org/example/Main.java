10b0dad14883b04062f13a7548038ffd86f66ec7
oeta <oeta.kumara@gmail.com>
2024-07-15 15:54:15 +0200

    Empty project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            // Load the XML file into a Document
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("./src/main/resources/example.xml");
+
+            // Create an XPath instance
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xpath = xPathFactory.newXPath();
+
+            // Compile and evaluate the XPath expression
+            String expression = "/employees/employee/name";
+            XPathExpression xPathExpression = xpath.compile(expression);
+
+            // Evaluate the expression and get the result
+            NodeList nodeList = (NodeList) xPathExpression.evaluate(doc, XPathConstants.NODESET);
+
+            // Iterate through the results and print them
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                System.out.println(nodeList.item(i).getTextContent());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
